84a416b756eabe4d8ed9eb58a2b6bc9b534522fd
Gary Mazzeo <gmazzeo@zingermans.com>
2022-02-26 17:10:09 -0500

    :tada: Initial commit

---
+++ features.js
@@ -0,0 +1,38 @@
+const code = document.querySelector('.code');
+const input = document.querySelector('.input');
+
+const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+
+function addNewLine(letter) {
+  return `${letter === 'z' ? '\n' : ''}`;
+}
+
+function iterateLetters(letter, i) {
+  const others = `[@not${letter.toUpperCase()} @NOT${letter} @Etc]`;
+  return `sub ${letter} ${others.repeat(i).replaceAll('][','] [')} ${letter}' by ${letter.toUpperCase()};
+sub ${letter.toUpperCase()} ${others.repeat(i).replaceAll('][','] [')} ${letter.toUpperCase()}' by ${letter}; ${addNewLine(letter)}`;
+}
+
+function makeFeature(iterations) {
+  let i = 1;
+  let output = alphabet.map(letter => 
+    `sub ${letter} ${letter}' by ${letter.toUpperCase()};
+sub ${letter.toUpperCase()} ${letter.toUpperCase()}' by ${letter}; ${addNewLine(letter)}`
+    ).join('\n');
+    while (i <= iterations) {
+      output += alphabet.map(letter => 
+        iterateLetters(letter, i)
+    ).join('\n');
+    i++;
+  }
+  code.innerText = `
+# ${iterations} iterations
+${output}`;
+};
+
+function handleSubmit(e) {
+  e.preventDefault();
+  makeFeature(input.iteration.value);
+}
+
+input.addEventListener('submit', handleSubmit);
