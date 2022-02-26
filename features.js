2f87e64d9a3fd77b352b7307cd9bc5398e14bd3e
Gary Mazzeo <gary.mazzeo@gmail.com>
2022-02-26 18:22:06 -0500

    :art: Remove semicolons

--- features.js
+++ features.js
@@ -1,38 +1,38 @@
-const code = document.querySelector('.code');
-const input = document.querySelector('.input');
+const code = document.querySelector('.code')
+const input = document.querySelector('.input')
 
-const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+const alphabet = [...'abcdefghijklmnopqrstuvwxyz']
 
 function addNewLine(letter) {
-  return `${letter === 'z' ? '\n' : ''}`;
+  return `${letter === 'z' ? '\n' : ''}`
 }
 
 function iterateLetters(letter, i) {
-  const others = `[@not${letter.toUpperCase()} @NOT${letter} @Etc]`;
-  return `sub ${letter} ${others.repeat(i).replaceAll('][','] [')} ${letter}' by ${letter.toUpperCase()};
-sub ${letter.toUpperCase()} ${others.repeat(i).replaceAll('][','] [')} ${letter.toUpperCase()}' by ${letter}; ${addNewLine(letter)}`;
+  const others = `[@not${letter.toUpperCase()} @NOT${letter} @Etc]`
+  return `sub ${letter} ${others.repeat(i).replaceAll('][','] [')} ${letter}' by ${letter.toUpperCase()}
+          sub ${letter.toUpperCase()} ${others.repeat(i).replaceAll('][','] [')} ${letter.toUpperCase()}' by ${letter}; ${addNewLine(letter)}`
 }
 
 function makeFeature(iterations) {
-  let i = 1;
+  let i = 1
   let output = alphabet.map(letter => 
-    `sub ${letter} ${letter}' by ${letter.toUpperCase()};
-sub ${letter.toUpperCase()} ${letter.toUpperCase()}' by ${letter}; ${addNewLine(letter)}`
-    ).join('\n');
+    `sub ${letter} ${letter}' by ${letter.toUpperCase()}
+     sub ${letter.toUpperCase()} ${letter.toUpperCase()}' by ${letter}; ${addNewLine(letter)}`
+    ).join('\n')
     while (i <= iterations) {
       output += alphabet.map(letter => 
         iterateLetters(letter, i)
-    ).join('\n');
-    i++;
+    ).join('\n')
+    i++
   }
   code.innerText = `
 # ${iterations} iterations
-${output}`;
-};
-
-function handleSubmit(e) {
-  e.preventDefault();
-  makeFeature(input.iteration.value);
+${output}`
 }
 
-input.addEventListener('submit', handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault()
+  makeFeature(input.iteration.value)
+}
+
+input.addEventListener('submit', handleSubmit)
